fe670d08e237dbda9ecd09385b1a4bf0f6f62bee
Salman Ahmed <say2salmanahmed@gmail.com>
2024-12-14 09:35:03 +0500

    Task 1

---
+++ Assignment 2/task1.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  // Create a list of names and print all names using list.
+
+  List<String> names = [
+    "Salman Ahmed",
+    "Shaheer Ahmed",
+    "Subhan Bashir",
+    "Suheer Khan"
+  ];
+
+  names.forEach((name) => print(name));
+}
