979f7fecb04bca0b53df8ef9d5768f00155feb46
Salman Ahmed <say2salmanahmed@gmail.com>
2024-12-14 09:39:21 +0500

    Task 2

---
+++ Assignment 2/task2.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  List<String> days = [];
+  days.addAll([
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ]);
+
+  days.forEach((day) => print(day));
+}
