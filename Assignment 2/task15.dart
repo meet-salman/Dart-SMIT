74bc8666252a31af3eb96fe55d1baef8394b5415
Salman Ahmed <say2salmanahmed@gmail.com>
2024-12-19 09:11:08 +0500

    Task 15

--- Assignment 2/task15.dart
+++ Assignment 2/task15.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() {
-  List<int> nums = [-11, -34, 784, 32, -76, 98, 211];
+  List<int> nums = [-11, -34, 784, 32, -76, 93, 211];
   List<int> newNums = filter(nums);
 
   print(newNums);
